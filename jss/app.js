42de2d67e92b32e7d07889af57f07f5746e79898
mtndbs <mtndbs@gmail.com>
2022-09-18 00:16:42 +0300

    first commit

---
+++ jss/app.js
@@ -0,0 +1,37 @@
+window.onload =()=>{
+  init()
+}
+// using correct api url '/?userId=1' to filter the json before fetching
+let url = 'https://jsonplaceholder.typicode.com/posts/?userId=1'
+let myData = ''
+
+
+//fetching using async function
+const init = async ( ) =>{
+  let resp = await fetch(url)
+  let data = await resp.json()
+  myRender(data)
+  return data
+}
+
+//coloring function using inside ternary operator
+const coloring = (id,color)=>{
+  document.getElementById(`id-${id}`).style.backgroundColor = color
+}  
+
+
+const myRender = (data) =>{
+  data.forEach((item)=> {
+    let myRoot = document.getElementById('root')  
+    
+    myRoot.innerHTML += `
+    <div style="width:70vw"> 
+    <h3 id="id-${item.id}">Id: ${item.id} </h3> 
+    <h3>Title: ${item.title}</h3>
+    <p> body: ${item.body}</P>
+    </div>
+    `
+    item.id % 2 === 0 ? coloring(item.id,'red'): coloring(item.id,'yellow')
+    item.id % 5 === 0 ? coloring(item.id,'hotpink'): console.log('i must type here somthing')
+  });
+    }
